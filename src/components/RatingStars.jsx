a945a696baf4049048ff63b18e3950bd9da13c76
Evan Geer <rust@evangeer.com>
2023-01-29 11:50:24 -0700

    clean up

---
+++ src/components/RatingStars.jsx
@@ -0,0 +1,31 @@
+import starImage from '../assets/star.png';
+import halfStarImage from '../assets/half-star.png';
+import React from 'react';
+
+export function RatingStars({ ratingData }) {
+  const { useEffect, useState } = React;
+  const [rating, setRating] = useState(ratingData);
+
+  function getStars(rating) {
+    console.log(`rating: ${rating}`);
+    let roundedRating = Math.round(rating * 2) / 2;
+    let halfStar = roundedRating % 2 > 0;
+    let starCount = Math.ceil(roundedRating);
+    console.log(starCount);
+
+    let stars = <>
+      {Array.from({ length: Math.ceil(roundedRating) }, (_, i) => {
+        return <img key={i} className='star' src={halfStar && i === starCount - 1 ? halfStarImage : starImage} />;
+      })}
+    </>;
+
+    return stars;
+  }
+
+  return (
+    <div className='scroll-box'>
+      {getStars(rating)}
+
+    </div>
+  );
+}

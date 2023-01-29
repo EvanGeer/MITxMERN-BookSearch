bca688530daa319179656a6e343bcec39803442c
Evan Geer <rust@evangeer.com>
2023-01-29 11:50:18 -0700

    clean up

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,61 +1,25 @@
-import starImage from '../assets/star.png';
-import halfStarImage from '../assets/half-star.png';
 import React from 'react';
 import { useAxios } from '../hooks/useAxios';
 import './book.css';
-
-const getAmazonImgUrls = (book) => {
-  let urls = book?.id_amazon?.filter(x => x?.length > 1)?.map(x => {
-      let imgSrc = `//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=${x}&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=evangeer-20&language=en_US`;
-      let hrefUrl = `https://www.amazon.com/dp/${x}?&linkCode=ll1&tag=evangeer-20&linkId=65c644c8c93f704026ee57deb7edd761&language=en_US&ref_=as_li_ss_tl`
-      return {imgSrc, hrefUrl};
-    });
-  return urls;
-}
-
-const getFirstAmazonImg = (book) => {
-  let urls = getAmazonImgUrls(book);
-  return urls?.[0]?.imgSrc;
-}
-
-function RatingStars({ratingData}) {
-  const { useEffect, useState } = React;
-  const [rating, setRating] = useState(ratingData);
-
-  // const [roundedRating, setRoundedRating] = useState(Math.round(rating*2)/2);
-  function getStars(rating) {
-    console.log(`rating: ${rating}`);
-    let roundedRating = Math.round(rating*2)/2;
-    let halfStar = roundedRating % 2 > 0;
-    let starCount = Math.ceil(roundedRating);
-    console.log(starCount)
-    let stars = <>
-      {Array.from({length: Math.ceil(roundedRating)}, (_, i) => {
-        return <img key={i} className='star' src={halfStar && i === starCount - 1 ? halfStarImage : starImage}/>
-      })}
-      </>
-      //{`star ${ ' half' : ''}`}
-    // let stars = 
-    //   <>
-    //     <img className={`star ${halfStar ? ' half' : ''}`} src='./star.png'/>
-    //     <img className={`star ${halfStar ? ' half' : ''}`} src='./star.png'/>
-    //     <img className={`star ${halfStar ? ' half' : ''}`} src='./star.png'/>
-    //   </>
-    return stars;
-  }
-
-  return (
-    <div className='scroll-box'>
-      {getStars(rating)}
-
-    </div>
-  )
-}
+import { RatingStars } from './RatingStars';
 
 export function Book({ book }) {
   const { useEffect, useState } = React;
 
-
+  const getAmazonImgUrls = (book) => {
+    let urls = book?.id_amazon?.filter(x => x?.length > 1)?.map(x => {
+        let imgSrc = `//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=${x}&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=evangeer-20&language=en_US`;
+        let hrefUrl = `https://www.amazon.com/dp/${x}?&linkCode=ll1&tag=evangeer-20&linkId=65c644c8c93f704026ee57deb7edd761&language=en_US&ref_=as_li_ss_tl`
+        return {imgSrc, hrefUrl};
+      });
+    return urls;
+  }
+  
+  const getFirstAmazonImg = (book) => {
+    let urls = getAmazonImgUrls(book);
+    return urls?.[0]?.imgSrc;
+  }
+  
   const [bookData, setBook] = useState(book);
   const [coverImage, setCoverImage] = useState((book?.cover_i)
       ? `https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg`
@@ -87,7 +51,6 @@ export function Book({ book }) {
       <img className="cover-image" src={coverImage} />
       <div className="book-details">
         <div className="book-title">{bookData.title ?? "not found..."}</div>
-          {/* <RatingStars ratingData={2}/> */}
         <div className="author">Author: 
           <text className='detail'> {bookData.author_name} </text>
           {(bookData.first_publish_year)
@@ -100,15 +63,7 @@ export function Book({ book }) {
           Ratings: {Math.round(ratingsData?.summary?.average*100)/100}&nbsp;
           <div className='detail'>({ratingsData?.summary?.count} reviews)</div> &nbsp;
           <RatingStars ratingData={ratingsData?.summary?.average}/>
-        </div>
-        }
-        {
-          // isLoading 
-          //   ? <>...</> : 
-          //   <>
-          //     {goodReadsData?.books?.reduce((runningTotal, current)=>runningTotal+=current)} reviews
-          //   </>
-        }
+        </div>}
         <div className='subhead'>
           Subjects:&nbsp;
           <div className='detail'>{book.subject?.some(x => x) ? book.subject.join(', ') : 'none found...'}</div>
@@ -134,7 +89,6 @@ export function Book({ book }) {
             </div>
           )
         }</div>
-
       </div>
     </div>
   );
